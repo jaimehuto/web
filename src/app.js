725adb1f0440430e9820f1608cf203a02316a2c1
jaimehuto <jhtdeveloper@gmail.com>
2021-03-14 16:13:55 +0100

    add scroll-reveal and remove clip-path classes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 
     // hide loader on window load
 
+    const sr = ScrollReveal();
+
     function loader () {
 
         var tl = gsap.timeline();
@@ -17,34 +19,35 @@
             y: "-100%",
             ease: Expo.easeInOut
         });
-        
+
         tl.from(".b-header__brand", {
             duration: 1,
             opacity: 0,
             y: 10,
-            ease: "back"
+            ease: "power4"
         });
 
-        tl.from(".c-hero__inner-overline", {
+        tl.from(".c-hero__inner-overline, .c-hero__inner-title, .c-hero__inner-text", {
             duration: 1,
             opacity: 0,
             y: 10,
-            ease: "back"
+            ease: "power4",
+            stagger: .75
         });
 
-        tl.from(".c-hero__inner-title", {
-            duration: 1,
-            opacity: 0,
-            y: 10,
-            ease: "back"
-        });
+        // tl.from(".c-hero__inner-title", {
+        //     duration: 1,
+        //     opacity: 0,
+        //     y: 10,
+        //     ease: "back"
+        // });
 
-        tl.from(".c-hero__inner-text", {
-            duration: 1,
-            opacity: 0,
-            y: 10,
-            ease: "back"
-        });
+        // tl.from(".c-hero__inner-text", {
+        //     duration: 1,
+        //     opacity: 0,
+        //     y: 10,
+        //     ease: "back"
+        // });
 
         tl.from(".c-hero__inner-arrow", {
             duration: 2,
@@ -70,7 +73,7 @@
 
             var jh_executed = false;
             
-            // Do action on CTRL + .
+            // Do action on CTRL + . 17
             if (e.keyCode == 190 && e.ctrlKey || e.keyCode == 46 && e.ctrlKey) {
 
                 $('.js-body').toggleClass('is-debug');
@@ -90,6 +93,30 @@
 
     }
 
+    function reveal(){
+
+        var slideUp = {
+            distance: '50%',
+            origin: 'bottom',
+            opacity: null,
+            duration: 1000
+        };
+        
+        sr.reveal('.slide-up', slideUp);
+
+        var fadeInUp = {
+            duration: 1000,
+            delay: 700,
+            distance: '20%',
+            origin: 'bottom',
+            opacity: null,
+            interval: 50
+        };
+
+        sr.reveal('.fade-in-up', fadeInUp);
+    }
+    
+
     function callFn(selector, fn, args){ if( $(selector).length > 0 ) fn(args); }
 
     $(window).on("load", function(){
@@ -99,6 +126,8 @@
         callFn(".js-scroll", scroll);
 
         callFn(".js-debug", jh_tools);
+
+        callFn(".js-reveal", reveal);
 
     });
 
